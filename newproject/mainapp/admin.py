5bb817203b5b2e6db71cf503928419fd86e7bb26
shohrux0113 <qonarov777@gmail.com>
2023-01-20 23:02:17 +0500

    test

--- newproject/mainapp/admin.py
+++ newproject/mainapp/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
-from .models import Starthome, Startteam,Startportfolio,Startme, Contact, Sity
+from .models import Starthome, Startteam,Startportfolio,Startme, Contact
 
 admin.site.register(Starthome)
 admin.site.register(Startteam)
 admin.site.register(Startportfolio)
 admin.site.register(Startme)
 admin.site.register(Contact)
-admin.site.register(Sity)
